53fc47778d2a5b8c3f5ad42da075d0105d4ef3d3
Shankar-Priyam <110894244+Shankar-Priyam@users.noreply.github.com>
2022-08-09 13:56:43 +0530

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+
+using namespace std;
+
+int glo= 6;
+void sum(){
+    int a;
+    cout<<glo;
+
+
+}
+
+
+
+int main(){
+
+    int glo=9;
+    glo= 51;
+    
+
+    int a= 33,b=2;
+    float pi=3.14;
+    char c =  'k';
+    sum();
+    cout<<glo;
+    
+    
+    // cout<<"Here the value of a is "<<a<< "\nThe value of b is" <<b;
+    // cout<<"\nThe value of pi is"<< pi;
+    // cout<<"\nThe value of c is "<< c;
+
+    return 0;
+}
---
+++ 3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num1 , num2;
+    cout<<"Enter the value of num1 :";
+    cin>>num1;
+    cout<<"Enter the value if num2:";
+    cin>>num2;
+    
+    cout<<" the sum of num1 and num2: "<< num1 + num2;
+
+    return 0;
+}
